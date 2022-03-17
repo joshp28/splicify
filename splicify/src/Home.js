d1d5ae5bb39d55a2ebad3ed7e1bf6827760cb64c
tranthai <tranthai@vt.edu>
2022-03-17 13:10:28 -0400

    New changes

--- splicify/src/Home.js
+++ splicify/src/Home.js
@@ -1,17 +1,20 @@
-import spliced from './AlbumSpliced.png';
+// import spliced from './AlbumSpliced.png';
 import './Home.css';
 import React from 'react';
 import Introduction from './Introduction';
 
 class Home extends React.Component {
   
+
+  
   render() {
+    let introStr = "This is in line 1.\nThis is in line 2.";
     return (
       <div className="Home">
         <header className="Home-header">
-          <Introduction text="Welcome to Splicify"/>
-          <img src={spliced} className="Home-logo" alt="spliced" />
-          <Introduction text="Welcome to Splicify"/>
+          <Introduction text="Welcome to Splicify" 
+            text2="Welcome to Splicify, this is an interactive experience that can visualize your top played songs."
+            text3="B:"/>
         </header>
         
         
@@ -23,6 +26,28 @@ class Home extends React.Component {
     );
   }
 }
+
+// OG version from Figma.
+// class Home extends React.Component {
+  
+//   render() {
+//     return (
+//       <div className="Home">
+//         <header className="Home-header">
+//           <Introduction text="Welcome to Splicify"/>
+//           <img src={spliced} className="Home-logo" alt="spliced" />
+//           <Introduction text="Welcome to Splicify"/>
+//         </header>
+        
+        
+        
+        
+
+//       </div>
+      
+//     );
+//   }
+// }
 
 
 
--- splicify/src/Introduction.js
+++ splicify/src/Introduction.js
@@ -6,7 +6,9 @@ class Introduction extends React.Component {
     render() {
         return (
             <div className="Introduction">
-                <h2>{this.props.text}</h2>
+                <h1>{this.props.text}</h1>
+                <h2>{this.props.text2}</h2>
+                <br />
             </div>
 
         )
