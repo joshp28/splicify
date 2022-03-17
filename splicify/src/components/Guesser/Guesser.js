f01372b416c598abd5f2c725bc664ec53d5af572
joshp28 <joshp28@vt.edu>
2022-03-17 13:09:13 -0400

    added guesser component

---
+++ splicify/src/components/Guesser/Guesser.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Guesser.module.css';
+
+const Guesser = () => (
+  <div className={styles.Guesser}>
+    Guesser Component
+  </div>
+);
+
+Guesser.propTypes = {};
+
+Guesser.defaultProps = {};
+
+export default Guesser;
