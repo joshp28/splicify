ac1cabca26c9303860c50b033c2ac775ba99e15d
tranthai <tranthai@vt.edu>
2022-04-19 15:30:40 -0400

    Get album cover from json file

--- splicify/src/components/Guesser/Guesser.js
+++ splicify/src/components/Guesser/Guesser.js
@@ -46,52 +46,58 @@ function setPlay(){
 class Guesser extends Component {
   componentDidMount() {
     this.cropImg();
-    var data = loaded;
-    var mydata = JSON.parse(data);
-    console.log(mydata.songs[0].song_artist);
+    
+    // console.log(mydata.songs[0].song_artist);
   } 
 
   state = {
     tries: 0,
     answers: [],
-    spotify: [{
-      "song_title": "Getcho Mans",
-      "song_artist": "Rich Brian", // Warren Hue;",
-      "image_url": "https://i.scdn.co/image/ab67616d0000b2732bb23b6519784ea65df29a8b"
-    },
-    {
-      "song_title": "edamame",
-      "song_artist": "bbno$", //; Rich Brian;",
-      "image_url": "https://i.scdn.co/image/ab67616d0000b273545a202ab06885cf2c6621ca"
-    },
-    {
-      "song_title": "damn Right",
-      "song_artist": "AUDREY NUNA",
-      "image_url": "https://i.scdn.co/image/ab67616d0000b27368db4b081db9b8a63483e052"
-    },
-    {
-      "song_title": "SOMEBODY",
-      "song_artist": "keshi",
-      "image_url": "https://i.scdn.co/image/ab67616d0000b2739922f27cab942f36da3f909e"
-    },
-    {
-      "song_title": "Lovers In The Night",
-      "song_artist": "Seori",
-      "image_url": "https://i.scdn.co/image/ab67616d0000b273f40a36b5fd31d9817c34b070"
-    }],
+    loaded,
     albumNum: 0
   };
+
+  // state = {
+  //   tries: 0,
+  //   answers: [],
+  //   spotify: [{
+  //     "song_title": "Getcho Mans",
+  //     "song_artist": "Rich Brian", // Warren Hue;",
+  //     "image_url": "https://i.scdn.co/image/ab67616d0000b2732bb23b6519784ea65df29a8b"
+  //   },
+  //   {
+  //     "song_title": "edamame",
+  //     "song_artist": "bbno$", //; Rich Brian;",
+  //     "image_url": "https://i.scdn.co/image/ab67616d0000b273545a202ab06885cf2c6621ca"
+  //   },
+  //   {
+  //     "song_title": "damn Right",
+  //     "song_artist": "AUDREY NUNA",
+  //     "image_url": "https://i.scdn.co/image/ab67616d0000b27368db4b081db9b8a63483e052"
+  //   },
+  //   {
+  //     "song_title": "SOMEBODY",
+  //     "song_artist": "keshi",
+  //     "image_url": "https://i.scdn.co/image/ab67616d0000b2739922f27cab942f36da3f909e"
+  //   },
+  //   {
+  //     "song_title": "Lovers In The Night",
+  //     "song_artist": "Seori",
+  //     "image_url": "https://i.scdn.co/image/ab67616d0000b273f40a36b5fd31d9817c34b070"
+  //   }],
+  //   albumNum: 0
+  // };
 
   saveInput = (e) => {
     this.setState({ input: e.target.value });
   };
 
   addNewItem = () => {
-    let { answers, input, spotify, albumNum, tries} = this.state;
+    let { answers, input, loaded, albumNum, tries} = this.state;
 
     // guess three times and activate hints on each wrong submission
     // guess it right
-    if (input != null && input.toLowerCase() === spotify[albumNum].song_title.toLowerCase()) {
+    if (input != null && input.toLowerCase() === loaded[albumNum].song_title.toLowerCase()) {
       answers.push("True"); 
       this.setState({ albumNum: this.state.albumNum + 1, tries: 0 });
 
@@ -113,36 +119,46 @@ class Guesser extends Component {
 
 
   cropImg() {
-    let { spotify } = this.state;
+    let { loaded } = this.state;
+    var data = loaded;
+    var mydata = JSON.parse(data);
+    
+
+    
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
 
     var image1 = new Image();
-    image1.src = spotify[0].image_url;
+    console.log("IMGSRC");
+    console.log(mydata);
+    console.log(mydata.songs);
+    console.log();
+    image1.src = mydata.songs[0].image_url;
+    console.log("IMGSRC");
     image1.onload = function () {
       ctx.drawImage(image1, (image1.width / 2) - 50, 0, 100, 500, 0, 0, 100, 500);
     }
 
     var image2 = new Image();
-    image2.src = spotify[1].image_url;
+    image2.src = mydata.songs[1].image_url;
     image2.onload = function () {
       ctx.drawImage(image2, (image2.width / 2) - 50, 0, 100, 500, 100, 0, 100, 500);
     }
 
     var image3 = new Image();
-    image3.src = spotify[2].image_url;
+    image3.src = mydata.songs[2].image_url;
     image3.onload = function () {
       ctx.drawImage(image3, (image3.width / 2) - 50, 0, 100, 500, 200, 0, 100, 500);
     }
 
     var image4 = new Image();
-    image4.src = spotify[3].image_url;
+    image4.src = mydata.songs[3].image_url;
     image4.onload = function () {
       ctx.drawImage(image4, (image4.width / 2) - 50, 0, 100, 500, 300, 0, 100, 500);
     }
 
     var image5 = new Image();
-    image5.src = spotify[4].image_url;
+    image5.src = mydata.songs[4].image_url;
     image5.onload = function () {
       ctx.drawImage(image5, (image5.width / 2) - 50, 0, 100, 500, 400, 0, 100, 500);
     }
